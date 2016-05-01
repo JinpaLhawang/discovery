c2a25ca62de099171ae16884010f7c84bc4c43a5
Nicholas Eden-Walker <nedenwalker@kenzan.com>
2016-05-01 19:53:24 -0400

    Logging.

--- src/main/java/com/jinpalhawang/jambudvipa/DiscoveryApplication.java
+++ src/main/java/com/jinpalhawang/jambudvipa/DiscoveryApplication.java
@@ -1,5 +1,7 @@
 package com.jinpalhawang.jambudvipa;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -8,8 +10,13 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class DiscoveryApplication {
 
+  private static final Logger log = LoggerFactory.getLogger(DiscoveryApplication.class);
+
   public static void main(String[] args) {
-    SpringApplication.run(DiscoveryApplication.class, args);
+    final SpringApplication app = new SpringApplication(DiscoveryApplication.class);
+    app.setLogStartupInfo(false);
+    app.run(args);
+    log.info(DiscoveryApplication.class.getSimpleName() + " started...");
   }
 
 }
