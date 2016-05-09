2480b63456cd1c8bb5694d367c7c429d8560436c
Nicholas Eden-Walker <nedenwalker@kenzan.com>
2016-05-09 13:29:16 -0400

    Revert logging./

--- src/main/java/com/jinpalhawang/jambudvipa/DiscoveryApplication.java
+++ src/main/java/com/jinpalhawang/jambudvipa/DiscoveryApplication.java
@@ -1,7 +1,5 @@
 package com.jinpalhawang.jambudvipa;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -10,13 +8,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class DiscoveryApplication {
 
-  private static final Logger log = LoggerFactory.getLogger(DiscoveryApplication.class);
-
   public static void main(String[] args) {
-    final SpringApplication app = new SpringApplication(DiscoveryApplication.class);
-    app.setLogStartupInfo(false);
-    app.run(args);
-    log.info(DiscoveryApplication.class.getSimpleName() + " started.");
+    SpringApplication.run(DiscoveryApplication.class, args);
   }
 
 }
